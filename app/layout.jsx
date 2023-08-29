738f4e90077122db7b0b2e8a41dd3f43df4a5979
SAUZET-Leny <leny.sauzet@gmail.com>
2023-08-29 22:29:15 +0200

    Layout & Links

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,9 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 
+// components
+import Navbar from './components/Navbar'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -11,7 +14,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        </body>
     </html>
   )
 }
