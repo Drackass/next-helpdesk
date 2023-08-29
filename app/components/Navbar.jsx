4638a65f34d0daea9078708861e7fb85376cdfb8
SAUZET-Leny <leny.sauzet@gmail.com>
2023-08-29 22:57:16 +0200

    Styles, Fonts & Images

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,17 @@
+import Image from 'next/image'
 import Link from 'next/link'
+import Logo from './dojo-logo.png'
 
 export default function Navbar() {
     return (
         <nav>
+            <Image
+                src={Logo}
+                alt='Helpdesk logo'
+                width={70}
+                quality={100}
+                placeholder='blur'
+            ></Image>
             <h1>Helpdesk</h1>
             <Link href="/">Dashboard</Link>
             <Link href="/tickets">Tickets</Link>
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,10 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Rubik } from 'next/font/google'
 
 // components
 import Navbar from './components/Navbar'
 
-const inter = Inter({ subsets: ['latin'] })
+const rubik = Rubik({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={rubik.className}>
         <Navbar />
         {children}
         </body>
