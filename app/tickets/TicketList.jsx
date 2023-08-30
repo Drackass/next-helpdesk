01193dc25e10c90553835e1a1f33483e30408b79
SAUZET-Leny <leny.sauzet@gmail.com>
2023-08-30 22:11:43 +0200

    Static Rendering

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -16,17 +16,18 @@ export default async function TicketList() {
     return (
         <>
             {tickets.map((ticket) => (
-                <div key={ticket.id} className="card my-5">
-                    <Link href={`/tickets/${ticket.id}`}>
-                    <h3>{ticket.title}</h3>
-                    <p>{ticket.body.slice(0,200)}...</p>
-                    <div className={`pill ${ticket.priority}`}>
-                        {ticket.priority} priority
+                <Link href={`/tickets/${ticket.id}`}>
+
+                    <div key={ticket.id} className="card my-5">
+                        <h3>{ticket.title}</h3>
+                        <p>{ticket.body.slice(0, 200)}...</p>
+                        <div className={`pill ${ticket.priority}`}>
+                            {ticket.priority} priority
+                        </div>
                     </div>
-                    </Link>
-                </div>
+                </Link>
             ))}
-            {tickets.lenght === 0 && (
+            {tickets.length === 0 && (
                 <p className="text-center">There are no open tickets !</p>
             )}
         </>
--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,9 +1,26 @@
+import { notFound } from "next/navigation"
+export const dynamicParams = true
+
+export async function generateStaticParams() {
+    const res = await fetch('http://localhost:4000/tickets')
+
+    const tickets = await res.json()
+
+    return tickets.map((ticket) => ({
+        id: ticket.id
+    }))
+}
+
 async function getTickets(id) {
     const res = await fetch('http://localhost:4000/tickets/' + id, {
         next: {
             revalidate: 60
         }
     })
+
+    if (!res.ok) {
+        notFound()
+    }
 
     return res.json()
 }
