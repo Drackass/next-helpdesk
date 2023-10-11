ab7f3d936bc6d33e6b1ec3db373be51dd167539c
SAUZET-Leny <leny.sauzet@gmail.com>
2023-10-11 13:26:39 +0200

    signup & login, protecting Routes & session Middleware

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,6 +1,17 @@
 import Link from "next/link";
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 
-export default function AuthLayout({children}) {
+export default async function AuthLayout({children}) {
+  const supabase = createServerComponentClient({ cookies })
+  const { data } = await supabase.auth.getSession()
+
+  if (data.session) {
+    redirect('/')
+    
+  }
+
   return (
     <>
         <nav>
--- app/(dashboard)/layout.jsx
+++ app/(dashboard)/layout.jsx
@@ -1,12 +1,23 @@
-import Navbar from "../components/Navbar";
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 
-export default function DashboardLayout({children}) {
+// components
+import Navbar from '@/app/components/Navbar'
+
+export default async function DashboardLayout({ children }) {
+  const supabase = createServerComponentClient({ cookies })
+  const { data } = await supabase.auth.getSession()
+
+  if (!data.session) {
+    redirect('/login')
+    
+  }
+
   return (
     <>
-        <Navbar/>
-        {children}
+      <Navbar user={data.session.user} />
+      {children}
     </>
   )
 }
-
-
